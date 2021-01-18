6596beff0ef7f7a20cb74a50c5677a6e05ec5361
ooooorobo <orobos654@gmail.com>
2021-01-18 19:23:40 +0900

    feat: Add createConnection with typeOrm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import Koa, {Context} from "koa";
+import Koa from "koa";
 import Router from "koa-router"
 
-import path from 'path'
 import dotenv from 'dotenv'
+
+import "reflect-metadata"
+import {createConnection} from "typeorm";
+import {TypeOrmConfig} from "./lib/orm-config";
 
 if (process.env.NODE_ENV === 'local') {
     dotenv.config({path: "script/env/.env.local"})
@@ -10,19 +13,25 @@ if (process.env.NODE_ENV === 'local') {
     dotenv.config({path: "script/env/.env.prod"})
 }
 
-const app = new Koa()
-const router = new Router()
+// [!] 다음 에러 발생 시 - Client does not support authentication protocol requested by server
+// 참고 - https://stackoverflow.com/a/50131831/14343719
+createConnection(TypeOrmConfig)
+    .then(async (connection) => {
+        const app = new Koa()
+        const router = new Router()
 
-router.get('/', (ctx, next) => {
-    ctx.body = 'hello world'
-})
+        router.get('/', (ctx, next) => {
+            ctx.body = 'hello world'
+        })
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+        app.use(router.routes())
+        app.use(router.allowedMethods())
 
-app.listen(4000, () => {
-    console.log('Todo-Server is listening to port 4000')
-    console.log(process.env.STORAGE_DATABASE_PORT)
-})
+        app.listen(4000, () => {
+            console.log('')
+            console.log(`Server started: ${new Date()}`)
+            console.log('Todo-Server is listening to port 4000')
+            console.log('')
+        })
 
-export default app;
+    }).catch((error) => console.log(error))
--- src/lib/orm-config.ts
+++ src/lib/orm-config.ts
@@ -1,24 +1,27 @@
-const STORAGE_DATABASE_HOST = process.env.STORAGE_DATABASE_HOST
-const STORAGE_DATABASE_PORT = process.env.STORAGE_DATABASE_PORT
-const STORAGE_DATABASE_USERNAME = process.env.STORAGE_DATABASE_USERNAME
-const STORAGE_DATABASE_PASSWORD = process.env.STORAGE_DATABASE_PASSWORD
-const STORAGE_DATABASE_DATABASE = process.env.STORAGE_DATABASE_DATABASE
+import {ConnectionOptions} from "typeorm";
 
-export const TypeOrmConfig = {
-    "type": "mysql",
-    "host": STORAGE_DATABASE_HOST,
-    "port": STORAGE_DATABASE_PORT,
-    "username": STORAGE_DATABASE_USERNAME,
-    "password": STORAGE_DATABASE_PASSWORD,
-    "database": STORAGE_DATABASE_DATABASE,
-    "synchronize": false,
-    "logging": false,
-    "entities": [
+const STORAGE_DATABASE_HOST = process.env.STORAGE_DATABASE_HOST || "localhost"
+const STORAGE_DATABASE_PORT = process.env.STORAGE_DATABASE_PORT || 3306
+const STORAGE_DATABASE_USERNAME = process.env.STORAGE_DATABASE_USERNAME || "root"
+const STORAGE_DATABASE_PASSWORD = process.env.STORAGE_DATABASE_PASSWORD || "password"
+const STORAGE_DATABASE_DATABASE = process.env.STORAGE_DATABASE_DATABASE || "todo"
+
+export const TypeOrmConfig: ConnectionOptions = {
+    type: "mysql",
+    host: STORAGE_DATABASE_HOST,
+    // @ts-ignore
+    port: STORAGE_DATABASE_PORT,
+    username: STORAGE_DATABASE_USERNAME,
+    password: STORAGE_DATABASE_PASSWORD,
+    database: STORAGE_DATABASE_DATABASE,
+    synchronize: false,
+    logging: false,
+    entities: [
         "src/entity/**/*.ts"
     ],
-    "migrations": [],
-    "subscribers": [],
-    "cli": {
-        "entitiesDir": "src/entity",
+    migrations: [],
+    subscribers: [],
+    cli: {
+        entitiesDir: "src/entity",
     }
 }
