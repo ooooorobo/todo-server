b903f738a80ff52cbd7460114b9fc73fcc73692b
ooooorobo <orobos654@gmail.com>
2021-01-18 18:45:12 +0900

    feat: Use typeorm
    orm-config 설정 파일, env 파일 연동 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import Koa, {Context} from "koa";
 import Router from "koa-router"
 
+import path from 'path'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV === 'local') {
+    dotenv.config({path: "script/env/.env.local"})
+} else if (process.env.NODE_ENV === 'prod') {
+    dotenv.config({path: "script/env/.env.prod"})
+}
+
 const app = new Koa()
 const router = new Router()
 
@@ -13,6 +22,7 @@ app.use(router.allowedMethods())
 
 app.listen(4000, () => {
     console.log('Todo-Server is listening to port 4000')
+    console.log(process.env.STORAGE_DATABASE_PORT)
 })
 
 export default app;
