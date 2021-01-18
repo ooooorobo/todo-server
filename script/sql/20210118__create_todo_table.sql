7f6f6fc1a14b9973f73c78035bdc3127b03aff0e
ooooorobo <orobos654@gmail.com>
2021-01-18 18:26:47 +0900

    fix: Add deleted_by, deleted_at column in TODO table DDL
    Todo 테이블 DDL 디테일 변경
    - create table IF NOT EXISTS 추가
    - 주석 표현 방식을 # 에서 -- 으로 변경
    - YesNo 방식 표현을 위한 칼럼 데이터형을 CHAR --> VARCHAR로 변경
    - CHARSET을 UTF8MB4로 변경 (utf8은 현재 UTF8MB3을 가리키고 있으나, 추후 UTF8MB4로 변경될 것임)

--- script/sql/20210118__create_todo_table.sql
+++ script/sql/20210118__create_todo_table.sql
@@ -1,27 +1,27 @@
-CREATE TABLE TODO
+CREATE TABLE IF NOT EXISTS TODO
 (
-    # PK
+    -- PK
     `id`             BIGINT UNSIGNED                                                  NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
 
-    # Columns
+    -- Columns
     `contents`       CHAR(255)                                                        NOT NULL,
-    `completed`      CHAR(4)    DEFAULT 'N'                                           NOT NULL,
+    `completed`      VARCHAR(4) DEFAULT 'N'                                           NOT NULL,
 
-    # FK
+    -- FK
     `target_user_id` BIGINT UNSIGNED                                                  NOT NULL,
     INDEX idx__TODO__target_user_id (target_user_id),
 
 
-    # Common
-    `locked`           VARCHAR(4) DEFAULT 'N'                                         NOT NULL,
+    -- Common
+    `locked`         VARCHAR(4) DEFAULT 'N'                                           NOT NULL,
 
     `created_by`     BIGINT UNSIGNED                                                  NOT NULL,
     `updated_by`     BIGINT UNSIGNED                                                  NOT NULL,
-    `deleted_by`     BIGINT UNSIGNED                                                  NOT NULL,
+    `deleted_by`     BIGINT UNSIGNED                                                  NULL,
 
     `created_at`     DATETIME   DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
     `updated_at`     DATETIME   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
-    `deleted_at`     DATETIME                                                         NOT NULL
+    `deleted_at`     DATETIME                                                         NULL
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = UTF8MB4;
